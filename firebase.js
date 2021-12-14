b80086ebccdd4e542861b80d1b6753faadc87887
Maha-code-star <74078213+Maha-code-star@users.noreply.github.com>
2021-12-14 13:05:45 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCQYZAWYwUcmT3osCnEDWfGMctPZzMq8l4",
+    authDomain: "nftforever-33.firebaseapp.com",
+    projectId: "nftforever-33",
+    storageBucket: "nftforever-33.appspot.com",
+    messagingSenderId: "767886409696",
+    appId: "1:767886409696:web:b09038aaa4dd8cddeab585",
+    measurementId: "G-3VE41NQNF9"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
